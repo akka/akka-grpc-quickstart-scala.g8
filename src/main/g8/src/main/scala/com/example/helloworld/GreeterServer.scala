8b42ab5f92db89b7e672df0650c7d01e0d9f1004
Johan Andrén <johan@markatta.com>
2024-05-17 13:45:43 +0200

    chore: Use the new cert loading utils in Akka HTTP

--- src/main/g8/src/main/scala/com/example/helloworld/GreeterServer.scala
+++ src/main/g8/src/main/scala/com/example/helloworld/GreeterServer.scala
@@ -72,31 +72,12 @@ class GreeterServer(system: ActorSystem[_]) {
   }
   //#server
 
-
-  private def serverHttpContext: HttpsConnectionContext = {
-    val privateKey =
-      DERPrivateKeyLoader.load(PEMDecoder.decode(readPrivateKeyPem()))
-    val fact = CertificateFactory.getInstance("X.509")
-    val cer = fact.generateCertificate(
-      classOf[GreeterServer].getResourceAsStream("/certs/server1.pem")
-    )
-    val ks = KeyStore.getInstance("PKCS12")
-    ks.load(null)
-    ks.setKeyEntry(
-      "private",
-      privateKey,
-      new Array[Char](0),
-      Array[Certificate](cer)
-    )
-    val keyManagerFactory = KeyManagerFactory.getInstance("SunX509")
-    keyManagerFactory.init(ks, null)
-    val context = SSLContext.getInstance("TLS")
-    context.init(keyManagerFactory.getKeyManagers, null, new SecureRandom)
-    ConnectionContext.httpsServer(context)
-  }
-
-  private def readPrivateKeyPem(): String =
-    Source.fromResource("certs/server1.key").mkString
+  private def serverHttpContext: HttpsConnectionContext =
+    ConnectionContext.httpsServer(SSLContextFactory.createSSLContextFromPem(
+      // Note filesystem paths, not classpath
+      Paths.get("src/main/resources/certs/server1.pem"),
+      Paths.get("src/main/resources/certs/server1.key")
+    ))
   //#server
 
 }
